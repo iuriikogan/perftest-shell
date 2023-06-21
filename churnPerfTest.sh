c71479cfd338afaf744bc19710e9850d9fa4ec42
Iurii Kogan <koganiurii@gmail.com>
2023-06-21 14:18:18 +0300

    update comments

--- churnPerfTest.sh
+++ churnPerfTest.sh
@@ -3,6 +3,7 @@
 NAMESPACE_PREFIX="small"
 NUM_FILES_TO_CHURN=5
 SIZE_OF_FILES="1Gb"
+COMMAND="rm /data/files/file\$i; dd if=/dev/random of=data/files/file$i bs=$SIZE_OF_FILES count=1; SLEEP 60"
 
 # Get the list of namespaces with the prefix
 namespaces=$(kubectl get namespaces | grep small | awk '{print $1}')
@@ -14,7 +15,7 @@ for namespaces in $namespaces; do
     echo "Deleting from Pod: $pod"
     for ((i=1; i<=$NUM_FILES_TO_CHURN; i++)); do
       # delete file and replace with new file from dev/random for NUM_FILES
-      kubectl exec -n "$namespaces" "$pod" -- "sh -C; rm /data/files/file\$i; dd if=/dev/random of=data/files/file$i bs=$SIZE_OF_FILES count=1; SLEEP 60"
+      kubectl exec -n "$namespaces" "$pod" -- "/bin/sh", "-c", $COMMAND; "done"
       echo "$i Files replaced."
 
     done
