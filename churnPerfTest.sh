45bf1443404c58458018b3903f9ca219123ddecf
Iurii Kogan <koganiurii@gmail.com>
2023-06-21 13:00:06 +0300

    update comments

--- churnPerfTest.sh
+++ churnPerfTest.sh
@@ -10,15 +10,21 @@ namespaces=$(kubectl get namespaces | grep small | awk '{print $1}')
 for namespaces in $namespaces; do
   # Loop through each pod in the namespace
   for pod in $(kubectl get pods -n "$namespaces" -o=jsonpath='{.items[*].metadata.name}'); do
-    echo "  Pod: $pod"
+    # Loop through $NUM_OF_FILES_TO_CHURN in the pod
+    echo "Deleting from Pod: $pod"
     for ((i=1; i<=$NUM_FILES_TO_CHURN; i++)); do
       # delete file and replace with new file from dev/random for NUM_FILES
       kubectl exec -n "$namespace" "$pod" -- "rm /data/files/file\$i &&
       dd if=/dev/random of=data/files/file$i bs=$SIZE_OF_FILES count=1 && SLEEP 60"
+
       echo "$i Files replaced."
+
     done
-   
+
+   echo "pod done."
+
   done
 
   echo "Namespace done."
+
 done
